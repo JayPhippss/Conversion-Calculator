8b05ff4a913043b2934e4961837116b560b01abd
JayPhippss <PhippsJaylin@gmail.com>
2017-07-26 08:56:24 -0500

    First Commit

---
+++ Converter Calculator/Converter Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Converter Calculator
+//
+//  Created by Jaylin Phipps on 7/26/17.
+//  Copyright © 2017 Jaylin Phipps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
