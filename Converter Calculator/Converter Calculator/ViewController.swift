370b7d4cddf22a9ed52f855d5babd27fa5e07915
JayPhippss <PhippsJaylin@gmail.com>
2017-07-26 11:01:35 -0500

    Converting from feet to miles and visa versa complete. still need more conversions and get the decimal to work

--- Converter Calculator/Converter Calculator/ViewController.swift
+++ Converter Calculator/Converter Calculator/ViewController.swift
@@ -9,7 +9,64 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var outputTextField: UITextField!
+    @IBOutlet weak var outputConvertLabel: UILabel!
+    @IBOutlet weak var inputConvertLabel: UILabel!
+    
+    var input1: Float = 0
+    var input2: Float = 0
+    var output1: Float = 0
 
+    @IBAction func numbers(_ sender: UIButton) {
+        inputTextField.text = inputTextField.text! + String(sender.tag - 1)
+        
+        input1 = Float(inputTextField.text!)!
+    }
+    
+    @IBAction func operations(_ sender: UIButton) {
+        if sender.tag == 12 {    //if C is pressed
+            inputTextField.text = ""
+            outputTextField.text = ""
+            inputConvertLabel.text = ""
+            outputConvertLabel.text = ""
+            input1 = 0
+        }
+        /*
+        if sender.tag == 11 { //if . is pressed
+            DECIMAL
+        }
+ */
+    }
+
+    @IBAction func converterBtn(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Conversions", message: "Select A Conversion", preferredStyle: UIAlertControllerStyle.actionSheet)
+        
+        alert.addAction(UIAlertAction(title: "Feet To Miles", style: UIAlertActionStyle.default , handler: {
+            (alertAction) -> Void in
+            self.inputConvertLabel.text = "Ft"
+            self.outputConvertLabel.text = "Mi"
+            
+            self.output1 = (self.input1 * Float(0.000189394))
+            self.outputTextField.text = String(self.output1)
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Miles To Feet", style: UIAlertActionStyle.default , handler: {
+            (alertAction) -> Void in
+            self.inputConvertLabel.text = "Mi"
+            self.outputConvertLabel.text = "Ft"
+            
+            self.output1 = (self.input1 * Float(5280))
+            self.outputTextField.text = String(self.output1)
+        }))
+        
+        
+        
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
